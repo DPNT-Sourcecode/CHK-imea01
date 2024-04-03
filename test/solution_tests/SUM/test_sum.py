7e0c1fc641b0a02dcf5c85c381b953fee1f891a8
sbx_user1051 <sbx_user1051@169.254.26.213>
2024-04-03 10:34:00 +0000

    Wed Apr 03 10:34:00 UTC 2024

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,17 +1,18 @@
+from solutions.SUM import sum_solution
+
+
+class TestSum():
+    def test_sum(self):
+        assert sum_solution.compute(1, 2) == 3
+
+# import unittest
 # from solutions.SUM import sum_solution
 
+# class TestMartianRobots(unittest.TestCase):
+#     def test_compute(self):
+#         self.assertEqual(sum_solution.compute(2, 1), 3)
 
-# class TestSum():
-#     def test_sum(self):
-#         assert sum_solution.compute(1, 2) == 3
+# if __name__ == '__main__':
+#     unittest.main()
 
-import unittest
-from lib.solutions.SUM import sum_solution
-
-class TestMartianRobots(unittest.TestCase):
-    def test_compute(self):
-        self.assertEqual(sum_solution.compute(2, 1), 3)
-
-if __name__ == '__main__':
-    unittest.main()
 
